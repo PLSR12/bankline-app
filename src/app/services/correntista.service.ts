9b43f54d7c41b7d0c48b3b4f4b4a0597f3d4adea
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-05 20:24:01 -0300

    create services

---
+++ src/app/services/correntista.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+const baseUrl = 'https://api--bankline.herokuapp.com'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CorrentistaService {
+  constructor (private http: HttpClient) {}
+
+  list (): Observable<any> {
+    return this.http.get(`${baseUrl}/correntistas`)
+  }
+
+  create (correntistas: any): Observable<any> {
+    return this.http.post(`${baseUrl}/correntistas`, correntistas)
+  }
+}
