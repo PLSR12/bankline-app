54cf1dc29b50a966ea6a9373141db45adad7dc69
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-05-10 14:07:02 -0300

    add import of ng-toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import {CurrencyMaskModule} from 'ng2-currency-mask'
+import { CurrencyMaskModule } from 'ng2-currency-mask'
+import { NgToastModule } from 'ng-angular-popup'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -13,10 +14,10 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { LOCALE_ID } from '@angular/core'
 import { registerLocaleData } from '@angular/common'
-import localePt from '@angular/common/locales/pt';
+import localePt from '@angular/common/locales/pt'
 import { CorrentistaComponent } from './components/correntista/correntista.component'
 
-registerLocaleData(localePt) 
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -31,9 +32,10 @@ registerLocaleData(localePt)
     NgbModule,
     FormsModule,
     HttpClientModule,
-    CurrencyMaskModule
+    CurrencyMaskModule,
+    NgToastModule
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
